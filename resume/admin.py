e930a942ae27d84307b104284cf7d724b1da9aac
Olim Bakaev <olimbakaev08@gmail.com>
2024-08-25 15:32:47 +0500

    created contact model

--- resume/admin.py
+++ resume/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from resume.models import contact
 # Register your models here.
+
+
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone']
+
+
+admin.site.register(contact, ContactUsAdmin)
--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
