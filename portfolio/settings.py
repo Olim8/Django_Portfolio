f9a7955fc916ed0ed1869e963eb8f795f97a07a8
Olim Bakaev <olimbakaev08@gmail.com>
2024-08-27 19:21:21 +0500

    email sending functionality added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -114,6 +114,13 @@ USE_I18N = True
 USE_TZ = True
 
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'gabistepanov08@gmail.com'
+EMAIL_HOST_PASSWORD = 'Gabi022000'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
