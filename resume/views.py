c2bede8a122119a5ecb4193b5157b4556c21b9b0
Olim Bakaev <olimbakaev08@gmail.com>
2024-08-28 18:59:26 +0500

    sending eamils 2

--- resume/views.py
+++ resume/views.py
@@ -4,7 +4,7 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 from .models import Contact
 from django.contrib import messages
 from django.core.mail import send_mail
-
+from django.conf import settings
 
 def home(request):
     return render(request, 'home.html')
@@ -60,10 +60,10 @@ def contact(request):
 
         # send email
         send_mail(
-            'Message from: ' + fname,
-            'Email: ' + femail,
-            'Contact: ' + fphone,
-            ['gabistepanov08@gmail.com'],
+            'Message from: ' + femail + '\n Name: ' + fname + '\n Phone: ' + fphone,
+            'Text: ' + fmessage,
+            femail,
+            settings.EMAIL_HOST_USER,
         )
         messages.info(request, 'Thanks For Contacting Me!')
         return redirect('/contact')
