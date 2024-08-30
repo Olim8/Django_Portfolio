f42a23014c7bd98273762b86a1be1c61e2d07cc8
Olim Bakaev <olimbakaev08@gmail.com>
2024-08-30 17:56:35 +0500

    some changes2

--- resume/admin.py
+++ resume/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Project
-# Register your models here.
-
+from .models import Contact, Project
 
 admin.site.register(Project)
+admin.site.register(Contact)
--- resume/models.py
+++ resume/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=100)
+    message = models.TextField()
+
+    class Meta:
+        verbose_name = 'contact us'
+        verbose_name_plural = 'contact us'
+
+    def __str__(self):
+        return f'Message from {self.name}'
+    
+
 class Project(models.Model):
     title = models.CharField(max_length=50)
     github_path = models.CharField(max_length=100)
